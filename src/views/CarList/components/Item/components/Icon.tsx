f89cd293d8002d20be1d80066f3a5e33cf93e749
Luan Bodner do Rosário <luan.rosario.bodner@gmail.com>
2022-07-20 19:04:24 -0300

    tests

--- src/views/CarList/components/Item/components/Icon.tsx
+++ src/views/CarList/components/Item/components/Icon.tsx
@@ -4,7 +4,7 @@ import { IconProps } from '../ItemProps'
 export function Icon(props: IconProps) {
   return (
     <div style={{ width: '10vw', height: '20vh' }}>
-      <img src={require(`${props.image}`)} alt='' style={{ height: '100%', width: '100%', objectFit: 'contain' }} />
+      <img src={props.image} alt='' style={{ height: '100%', width: '100%', objectFit: 'contain' }} />
     </div>
   )
 }
